e415979dcaf0c6fdd24a63f8d427cad4398966bd
cakkac <cakkac@users.noreply.github.com>
2018-10-08 19:18:00 +0200

    Update Command_Contrail-Install.sh

--- Command_Contrail-Install.sh
+++ Command_Contrail-Install.sh
@@ -4,23 +4,30 @@
 # Reference: https://www.juniper.net/documentation/en_US/contrail5.0/topics/example/install-contrail-command.html
 # Date written 2018 October 8
 
-#need wget and change chmod for script
+######### HELP ##########
+# need wget and change chmod for script
 # install wget package to retrieve files from Github
-# yum install -y wget
+#   yum install -y wget
+#  and then dowload the script: wget https://github.com/cakkac/contrail/raw/master/Command_Contrail-Install.sh
 # to run this script: sh Contrail_Command-Install.sh
+#########################
 
 
 # retrieve login/pwd for hub.juniper.net
-printf "Repository hub.juniper.net Authentication \n"
-read -p 'Username: ' CONTAINER_REGISTRY_USERNAME
-read -sp 'Password: ' CONTAINER_REGISTRY_PASSWORD
+printf "\e[1m Repository hub.juniper.net Authentication \e[21 \n"
+printf "\e[4m Username: "
+read -p CONTAINER_REGISTRY_USERNAME
+printf "\n Password: \e[24m"
+read -p CONTAINER_REGISTRY_PASSWORD
 echo
 
+
 # retrieve parameters about the server
-printf "Server details for Contrail Command installation (Mandatory to fill them): \n"
-printf "login to run this shell should be root :) \n"
-read -p 'IP@ or FQDN : ' NewServer1IP
-read -p 'NTP server IP@/FQDN (you can use public : 0.pool.ntp.org): ' NewNTPServer
+printf "\e[1m Server details for Contrail Command installation (Mandatory to fill them): \n"
+printf "login to run this shell should be root :) \e[21 \n"
+read -p 'Server IP@ or FQDN : ' NewServer1IP
+read -e -p 'NTP server IP@/FQDN (keep the following value or delete & replace with another IP/FQDN): ' -i "0.pool.ntp.org" NewNTPServer
+
 
 # Login/pwd on the server should be root/c0ntrail123 - not mandatory for Contrail Command installation but for Contrail Networking..
 # Allow Root login via SSH
@@ -45,7 +52,7 @@ systemctl start docker
 printf  "\n Authenticating to hub.juniper.net.\n"
 docker login hub.juniper.net --username $CONTAINER_REGISTRY_USERNAME --password $CONTAINER_REGISTRY_PASSWORD
 printf  "Pulling Contrail Init Containers.\n"
-echo "Output of 'docker pull hub.juniper.net/contrail/contrail-node-init:5.0.1-0.214'"
+docker pull hub.juniper.net/contrail/contrail-node-init:5.0.1-0.214
 sleep 3
 
 # get and edit command_servers.yml file to reflect the user entered data
@@ -72,5 +79,8 @@ printf "\n Ansible playbook for Contrail Command started"
 docker run -t --net host -v ~/command_servers.yml:/command_servers.yml -d --privileged --name contrail_command_deployer hub.juniper.net/contrail/contrail-command-deployer:5.0.1-0.214
 
 
-Echo "\n Command Contrail installation in progress:"
+echo "\n Command Contrail installation in progress:"
 docker logs -f contrail_command_deployer
+
+echo "\n Contrail Command accessible: "
+echo "https://"$NewServer1IP":9091"
